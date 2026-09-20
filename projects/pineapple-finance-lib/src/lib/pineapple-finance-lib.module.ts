6c556140823b9ca8711dd15c39f120032558de3b
ongzhixian <zhixian@hotmail.com>
2026-09-20 08:24:23 +0800

    Add library stub

--- projects/pineapple-finance-lib/src/lib/pineapple-finance-lib.module.ts
+++ projects/pineapple-finance-lib/src/lib/pineapple-finance-lib.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { PineappleFinanceLibComponent } from './pineapple-finance-lib.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 
 
 @NgModule({
   declarations: [
-    PineappleFinanceLibComponent
+    PineappleFinanceLibComponent,
+    DashboardComponent
   ],
   imports: [
   ],
